ef6095b5c153d3730dfa72309763936dd081993f
Barkev Reisig <redblackrider@gmail.com>
2024-05-12 15:01:42 -0400

    Added database framework for stat tracking

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,3 +1,5 @@
+import fbapp from '../database/diceStatsDB.js';
+import { getFirestore, collection, getDocs, doc, setDoc, Timestamp } from "firebase/firestore"; 
 import { useState } from 'react';
 import DiceRoller from './DiceRoller.js';
 import Statistics from './Statistics.js';
@@ -5,10 +7,21 @@ import Statistics from './Statistics.js';
 function Dice() {
   const [rollCount, setRollCount] = useState(0);
   const [resultTotal, setResultTotal] = useState(0);
+	const db = getFirestore(fbapp);
+
+	async function updateDBStats(result) {
+		const date = new Date();
+		const timestamp = Timestamp.fromDate(date);
+		await setDoc(doc(db, "roll-results", `${date}`), {
+			"roll-value": result,
+			date: timestamp
+		});
+	}		
 
   function updateStats(result) {
     setResultTotal(resultTotal + result);
     setRollCount(rollCount + 1);
+		updateDBStats(result);
   }
 
   return (
---
+++ src/database/diceStatsDB.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBq3Sp02wjPMu6ibIGP-9A1W3CJ27B5c60",
+  authDomain: "de2-dice-app-db.firebaseapp.com",
+  projectId: "de2-dice-app-db",
+  storageBucket: "de2-dice-app-db.appspot.com",
+  messagingSenderId: "228429744425",
+  appId: "1:228429744425:web:cfa9f3981e9f44d3e3cf38"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
