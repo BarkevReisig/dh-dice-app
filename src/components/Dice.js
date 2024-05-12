e5ceae4a9136758660cd8beb8a7c64d0895228ab
Barkev Reisig <redblackrider@gmail.com>
2024-05-12 16:43:38 -0400

    Simplified ids for db

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,5 +1,5 @@
 import fbapp from '../database/diceStatsDB.js';
-import { getFirestore, doc, setDoc, Timestamp, getDoc } from "firebase/firestore"; 
+import { getFirestore, Timestamp, collection, addDoc } from 'firebase/firestore'; 
 import { useState } from 'react';
 import DiceRoller from './DiceRoller.js';
 import Statistics from './Statistics.js';
@@ -11,14 +11,9 @@ function Dice() {
 
 	async function updateDBStats(result) {
 		const timestamp = Timestamp.fromDate(new Date());
-		const appData = await getDoc(doc(db, "backend", "app-data"));
-		const resultId = appData.get("id-counter") + 1;
-		setDoc(doc(db, "backend", "app-data"), {
-			"id-counter": resultId
-		});
-		setDoc(doc(db, "roll-results", `${resultId}`), {
-			"roll-value": result,
-			"date": timestamp
+		const res = await addDoc(collection(db, 'roll-results'), {
+			'roll-value': result,
+			'date': timestamp,
 		});
 	}		
 
